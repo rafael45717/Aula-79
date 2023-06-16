4215366f4a28dcdd9590dac2b0708c5f332bff5c
rafael45717 <120226885+rafael45717@users.noreply.github.com>
2023-06-16 20:18:28 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+var arrayNomesConvidados = [];
+function botaoEnviar() {
+    var nome1 = document.getElementById ("nomePrimeiraPessoa").value;
+    var nome2 = document.getElementById ("nomeSegundaPessoa").value;
+    var nome3 = document.getElementById ("nomeTerceiraPessoa").value;
+    var nome4 = document.getElementById ("nomeQuartaPessoa").value;
+
+
+
+arrayNomesConvidados.push(nome1);
+arrayNomesConvidados.push(nome2);
+arrayNomesConvidados.push(nome3);
+arrayNomesConvidados.push(nome4);
+console.log(arrayNomesConvidados);
+
+
+document.getElementById("NomesConvidados").innerHTML = arrayNomesConvidados;
+document.getElementById("")
+document.getElementById("")
+
+
+
+
+
+
+}
